f4005d2f2de51e65b37a16c861b36e1ff5a1e883
Christopher Kilding <user@fake>
2018-09-24 22:20:37 +0100

    WIP try a 5dp capped approach

--- src/main/java/org/loadtest4j/drivers/wrk/dto/Latency.java
+++ src/main/java/org/loadtest4j/drivers/wrk/dto/Latency.java
@@ -3,14 +3,18 @@ package org.loadtest4j.drivers.wrk.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.TreeMap;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Latency {
+    /**
+     * A mapping of (the percentile, decimal) to (the percentile value).
+     */
     @JsonProperty
-    private Map<Integer, Long> percentiles;
+    private TreeMap<BigDecimal, Long> percentiles;
 
-    public Map<Integer, Long> getPercentiles() {
+    public TreeMap<BigDecimal, Long> getPercentiles() {
         return percentiles;
     }
 }
